944326dcde984457b1b8c027000c77a934a18174
Luciano <lucianodacunha.net@gmail.com>
2025-11-27 23:41:56 -0300

    feat: Criar funcionalidades de criar e listar usuários e conta corrente.

--- 03-otimizando-o-sistema-bancario-com-funcoes-python/src/01_sistema_bancario.py
+++ 03-otimizando-o-sistema-bancario-com-funcoes-python/src/01_sistema_bancario.py
@@ -72,11 +72,8 @@ O programa deve armazenar os usuários em uma lista, um usuário é composto por
 - nome
 - data de nascimento
 - cpf
-- endereço: É uma string com o formato:
-    - logradouro
-    - número
-    - bairro
-    - cidade/sigla uf.
+- endereço: É uma string com o formato: 
+    => logradouro, número - bairro - cidade/uf.
 Não podemos cadastrar 2 usuários com o mesmo cpf.
 
 Criar conta corrente
@@ -102,35 +99,66 @@ def main():
     extrato = ""
     numero_saques = 0
     LIMITE_SAQUES = 3
+    usuarios = {}
+    contas = {}
 
     while True:
+        opcao = menu_conta()
 
-        opcao = menu()
+        if opcao == "u": # Criar usuários
+            usuarios, contas, _ = criar_usuario(usuarios, contas)
+        elif opcao == "c": # Criar contas
+            contas, _ = _criar_conta_corrente(contas, usuarios=usuarios)        
+        elif opcao == "l": # Listar usuários
+            listar_usuários(usuarios)
+        elif opcao == "r": # Listar contas
+            listar_contas(contas)
+        elif opcao == "o": # Operar contas
+            while True:
 
-        if opcao == "d":
-            valor = float(input("Informe o valor do depósito: "))
+                opcao = menu_operacoes()
 
-            saldo, extrato = deposito(saldo, valor, extrato)
 
-        elif opcao == "s":
-            valor = float(input("Informe o valor do saque: "))
+                if opcao == "d":
+                    valor = float(input("Informe o valor do depósito: "))
 
-            saldo, extrato, numero_saques = saque(saldo=saldo, valor=valor,
-                extrato=extrato, limite=limite, numero_saques=numero_saques, 
-                limite_saques=LIMITE_SAQUES)
+                    saldo, extrato = deposito(saldo, valor, extrato)
 
-        elif opcao == "e":
-            _extrato(saldo, extrato=extrato)
+                elif opcao == "s":
+                    valor = float(input("Informe o valor do saque: "))
 
-        elif opcao == "q":
+                    saldo, extrato, numero_saques = saque(saldo=saldo, valor=valor,
+                        extrato=extrato, limite=limite, numero_saques=numero_saques, 
+                        limite_saques=LIMITE_SAQUES)
+
+                elif opcao == "e":
+                    _extrato(saldo, extrato=extrato)
+
+                elif opcao == "q":
+                    return
+
+                else:
+                    print("Opção inválida, "
+                        + "por favor informe novamente a operação desejada.")
+        elif opcao == "s": # Sair
             break
-
         else:
-            print("Operação inválida, "
-                + "por favor selecione novamente a operação desejada.")
+            print("Opção inválida, "
+                + "por favor informe novamente a opção desejada.")
 
 
-def menu():
+def menu_conta():
+    return input("\n"
+        + "=> Criar [u]suário\n"
+        + "=> [c]riar conta\n"
+        + "=> [l]istar usuários\n"
+        + "=> Lista[r] contas\n"
+        + "=> [o]perar contas\n"
+        + "=> [s]air\n\n"
+        + "=> ")
+
+
+def menu_operacoes():
     return input("\n"
         + "[d] Depositar\n"
         + "[s] Sacar\n"
@@ -163,7 +191,7 @@ def saque(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     return saldo, extrato, numero_saques
 
 
-def deposito(saldo, valor, extrato):
+def deposito(saldo, valor, extrato, /):
     if valor > 0:
         saldo += valor
         extrato += f"Depósito: R$ {valor:.2f}\n"        
@@ -179,6 +207,107 @@ def _extrato(saldo, *, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("".center(42, "="))
     
+
+def criar_usuario(usuarios, contas, /, conta=None):
+    """Cria um usuário e caso nenhuma conta for informada, uma conta corrente
+    será criada e associada ao usuário atual.
+    """
+    usuario = {}
+
+    print("\nEntre com os dados do usuário: ")
+    nome = input("    Nome: ")
+    data_nascimento = input("    Data de nascimento: ")
+    cpf = input("    CPF: ")
+    print("    Entre com os dados do endereço do usuário: ")
+    logradouro = input("        Logradouro: ")
+    numero = input("        Número: ")
+    bairro = input("        Bairro: ")
+    cidade = input("        Cidade: ")
+    uf = input("        UF: ")
+    endereco = f"{logradouro}, {numero} - {bairro} - {cidade}/{uf}"
+
+    usuario[cpf] = {
+        "nome": nome,
+        "nascimento": data_nascimento,
+        "endereco": endereco
+    }
+
+    if not conta:
+        input("\nUsuário criado sem um conta associada."
+                + " Pressione enter para criar uma conta corrente... ")
+        contas, conta = _criar_conta_corrente(contas, usuario, usuarios)
+        print(contas, conta)
+        input("_")
+        print("\nConta corrente criada com sucesso!\n")
+
+    list(conta.values())[0]["usuario"] = cpf
+    list(usuario.values())[0]["contas"] = conta
+    usuarios.update(usuario)
+
+    return usuarios, contas, usuario
+
+
+def _criar_conta_corrente(contas, /, usuario={}, usuarios=None):
+    """Cria conta corrente, caso nenhum usuário for informado, cria um usuário
+    e associa a conta atual.
+    """
+    conta = {}
+    numero = len(contas) + 1
+    agencia = "0001"
+    # usuario = None if not usuario else list(usuario.keys())[0]
+    conta[numero] = {"agencia": agencia}
+
+    print(f"\nConta criada com sucesso: {conta}")
+    print()
+
+    if not usuario:
+        while True:
+            opcao = input("Atribua um usuário à nova conta: "
+                        + "\n   [i]nformar um usuário (CPF) existente:"
+                        + "\n   [c]riar um novo usuário: "
+                        + "\n"
+                        + "\n   ==> ")
+            if opcao == "i":
+                cpf = input("Informe o CPF do usuário desta conta: ")
+                if cpf in usuarios:
+                    usuario = {}
+                    usuario[cpf] = usuarios.get(cpf)
+                    list(usuario.values())[0].get("contas").update(conta)
+                    break
+                else:
+                    print("CPF não localizado!")
+            elif opcao == "c":
+                usuarios, contas, usuario = criar_usuario(usuarios, contas, 
+                                                conta=conta)
+                break
+            else:
+                print("Opção inválida, informe novamente...")
+
+    cpf = list(usuario.keys())[0]
+    list(conta.values())[0]["usuario"] = cpf
+    contas.update(conta)
+
+    return contas, conta
+
+
+def listar_usuários(usuarios, /):
+    cpfs = list(usuarios.keys())
+
+    for cpf in cpfs:
+        dados = usuarios.get(cpf)
+        usuario = (f"Nome: {dados['nome']}, Nascimento: {dados['nascimento']}, "
+                 + f"Endereço: {dados['endereco']}, CPF: {cpf}.")
+        print(usuario)
+
+
+def listar_contas(contas, /):
+    numeros = list(contas.keys())
+
+    for numero in numeros:
+        dados = contas.get(numero)
+        conta = (f"Número: {numero}, Agência: {dados['agencia']}, "
+               + f"CPF:  {dados['usuario']}.")
+        print(conta)
 
 
 if __name__ == "__main__":
