c8eda452ff5334099a5bfeacee6bf2655f83141f
Luciano <lucianodacunha.net@gmail.com>
2025-12-08 11:31:01 -0300

    Primeiros passos com fastapi: Rotas e endpoints em fastapi - path parameter.

--- 04-primeiros-passos-com-fastapi/src/main.py
+++ 04-primeiros-passos-com-fastapi/src/main.py
@@ -1,8 +1,13 @@
+from datetime import UTC, datetime
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_route():
-    return {"message": "Hello World!!!"}
+@app.get('/post/{framework}')
+def read_posts(framework: str):
+    return {"posts": [
+        {"title": f"Criando uma aplicação com {framework}", "date": datetime.now(UTC)},
+        {"title": f"Internacionalizando uma app {framework}", "date": datetime.now(UTC)}
+    ]}
