67ca08001dc7e8f59120c8432ff355350af5b38d
Luciano <lucianodacunha.net@gmail.com>
2025-12-08 15:56:38 -0300

    Primeiros passos com fastapi: Organizando as rotas com apirouter.

--- 04-primeiros-passos-com-fastapi/src/controllers/post.py
+++ 04-primeiros-passos-com-fastapi/src/controllers/post.py
@@ -1,11 +1,12 @@
 from datetime import UTC, datetime
 from typing import Annotated
 
-from fastapi import Cookie, Header, Response, status
+from fastapi import APIRouter, Cookie, Header, Response, status
 from schemas.post import PostIn
 from views.post import PostOut
 
-from main import app
+
+router = APIRouter(prefix="/posts")
 
 fake_db = [
     {"title": f"Criando uma aplicação com Django", "date": datetime.now(UTC), "published": True},
@@ -15,23 +16,24 @@ fake_db = [
 ]
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def create_posts(post: PostIn):
     fake_db.append(post.model_dump())
     return post
 
 
-@app.get("/posts", response_model=list[PostOut])
+@router.get("/", response_model=list[PostOut])
 def read_posts(response: Response, published: bool, limit: int, skip: int = 0, 
                ads_id: Annotated[str | None, Cookie()] = None,
                user_agent: Annotated[str | None, Header()] = None):
     response.set_cookie(key="user", value="user@gmail.com")
     print(f"Cookie {ads_id}")
     print(f"User-Agent {user_agent}")
-    return [post for post in fake_db[skip: skip + limit] if post["published"] is published]
+    tail = skip + limit
+    return [post for post in fake_db[skip: tail] if post["published"] is published]
 
 
-@app.get('/post/{framework}', response_model=PostOut)
+@router.get('/posts/{framework}', response_model=PostOut)
 def read_framework_posts(framework: str):
     return {"posts": [
         {"title": f"Criando uma aplicação com {framework}", "date": datetime.now(UTC)},
--- 04-primeiros-passos-com-fastapi/src/main.py
+++ 04-primeiros-passos-com-fastapi/src/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
+from controllers import post
+
+
 app = FastAPI()
+app.include_router(post.router)
